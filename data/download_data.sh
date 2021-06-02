41452f447284491cf8ade8e09f3bc4e314ec64f7
Giannis Karamanolakis <giannis.karamanolakis@gmail.com>
2021-06-02 12:00:29 -0400

    add ISWD scripts

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 echo "downloading datasets"
+
+## ** SemEval Dataset **
 wget -O semeval.zip https://www.dropbox.com/s/b5c3nk54pr85836/semeval.zip?dl=0;
 unzip semeval.zip
 
+# pre-computed BERT embeddings 
+wget -O semeval_bert.zip https://www.dropbox.com/s/pwgmzrzid8774de/semeval_bert.zip?dl=0;
+mkdir pretrained_bert
+unzip semeval_bert.zip -d pretrained_bert/semeval
+
+
+## ** OPOSUM Dataset **
+# To download our pre-processed version, uncomment the next lines of code (datafiles are large)
+#  wget -O oposum.zip https://www.dropbox.com/s/u1hmjsuwy8zuhfj/oposum.zip?dl=0;
+#  unzip oposum.zip -d oposum
+
+# pre-computed BERT embeddings
+#  wget -O bags_and_cases.zip https://www.dropbox.com/s/teoq57uhve0jszi/bags_and_cases.zip?dl=0;
+#  wget -O bluetooth.zip https://www.dropbox.com/s/9qg8ikrxkq7v3aq/bluetooth.zip?dl=0;
+#  wget -O boots.zip https://www.dropbox.com/s/u3v5f7fyhxnvkv8/boots.zip?dl=0;
+#  wget -O keyboards.zip https://www.dropbox.com/s/pcj5auzxtb094pm/keyboards.zip?dl=0;
+#  wget -O tv.zip https://www.dropbox.com/s/0qa7nwbrs19etyl/tv.zip?dl=0;
+#  wget -O vacuums.zip https://www.dropbox.com/s/48jk5vxpg53s5ny/vacuums.zip?dl=0;
+
+#  unzip bags_and_cases.zip -d pretrained_bert/oposum
+#  unzip bluetooth.zip -d pretrained_bert/oposum
+#  unzip boots.zip -d pretrained_bert/oposum
+#  unzip keyboards.zip -d pretrained_bert/oposum
+#  unzip tv.zip -d pretrained_bert/oposum
+#  unzip vacuums.zip -d pretrained_bert/oposum
 
--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -7,15 +7,21 @@ mkdir ../experiments/
 ## Restaurant Reviews in SemEval dataset (Table 4)
 logdir="../experiments/Restaurants"
 mkdir $logdir
+
 # Student-W2V
 ./run_5_times.sh semeval --lr 0.01 --no_seed_weights --disable_gpu --logdir $logdir/$(date +'%h%d_%H-%M-%S-%N')_W2V --batch_size 64 --emb_dropout 0.2 --loss SmoothCrossEntropy
+
 # Student-BERT
-#./run_5_times.sh semeval --lr 0.01 --no_seed_weights --disable_gpu --logdir $logdir/$(date +'%h%d_%H-%M-%S-%N')_BERT --batch_size 64 --emb_dropout 0.2 --loss SmoothCrossEntropy --use_bert
+./run_5_times.sh semeval --lr 0.01 --no_seed_weights --disable_gpu --logdir $logdir/$(date +'%h%d_%H-%M-%S-%N')_BERT --batch_size 64 --emb_dropout 0.2 --loss SmoothCrossEntropy --use_bert
+
 
 ## Amazon Product Reviews in OPOSUM dataset (Table 3)
-#logdir="../experiments/Products"
-mkdir $logdir
+# To run OPOSUM scripts make sure you first download the OPOSUM dataset (see data/download_data.sh) and then uncomment the following lines
+#  logdir="../experiments/Products"
+#  mkdir $logdir
+
 # Student-W2V
-#./run_5_times.sh pairs --lr 0.0005 --no_seed_weights --logdir $logdir/$(date +'%h%d_%H-%M-%S-%N')_W2V --emb_dropout 0.5 --weight_decay 0.1 --loss SmoothCrossEntropy
+#  ./run_5_times.sh pairs --lr 0.0005 --no_seed_weights --logdir $logdir/$(date +'%h%d_%H-%M-%S-%N')_W2V --emb_dropout 0.5 --weight_decay 0.1 --loss SmoothCrossEntropy
+
 # Student-BERT
-#./run_5_times.sh pairs --lr 0.00005 --no_seed_weights --logdir $logdir/$(date +'%h%d_%H-%M-%S-%N')_BERT --emb_dropout 0.5 --weight_decay 0.0001 --use_bert
+#  ./run_5_times.sh pairs --lr 0.00005 --no_seed_weights --logdir $logdir/$(date +'%h%d_%H-%M-%S-%N')_BERT --emb_dropout 0.5 --weight_decay 0.0001 --use_bert
